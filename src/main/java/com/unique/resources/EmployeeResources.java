3fd78d6a93bcf92c1b6ac6f461d461c935aa1c0c
Sonu <s.gupta@axelor.com>
2023-02-03 16:52:41 +0530

    Display

--- src/main/java/com/unique/resources/EmployeeResources.java
+++ src/main/java/com/unique/resources/EmployeeResources.java
@@ -8,6 +8,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.jboss.resteasy.plugins.providers.html.View;
+
 import com.google.inject.Inject;
 import com.unique.Db.Employee;
 import com.unique.service.EmployeeService;
@@ -17,11 +19,21 @@ public class EmployeeResources {
 		@Inject
 		EmployeeService es;
 	
-	@GET	
-	@Path("/list")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getListEmp() {
-			List<Employee> list  = es.getAllPersons();
-		return Response.ok(list).build();
-	}
+//	@GET	
+//	@Path("/list")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response getListEmp() {
+//			List<Employee> list  = es.getAllPersons();
+//		return Response.ok(list).build();
+//	}
+
+	
+		@GET	
+		@Path("/list")
+		public View getListEmp() {
+				List<Employee> list  = es.getAllPersons();
+				return new View("/view.jsp", list,"list");
+		}
+	
+	
 }
---
+++ src/main/webapp/view.jsp
@@ -0,0 +1,39 @@
+<%@page import="java.util.List"%>
+<%@page import="com.unique.Db.Employee"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Our Employee</title>
+</head>
+<body>
+	<h1 align="center">Our Valuable Employee</h1>
+
+	<table border=2 align="center">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Phone</th>
+        </tr>
+			  <% for (Employee e :(List<Employee>)request.getAttribute("list")) { %>
+			<tr width="42" height="42">
+				<td><%=e.getId()%></td>
+				<td><%=e.getName()%></td>
+				<td><%=e.getCity()%></td>
+				<td><%=e.getState()%></td>
+				<td><%=e.getPhone()%></td>
+			</tr>
+			<%
+			}
+			%>
+		</tbody>
+	</table>
+	
+
+</body>
+</html>
+
